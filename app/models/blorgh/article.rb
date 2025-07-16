1b4f1492b542c6668da0834b48fe769cdcfec0f2
Ridhwana <Ridhwana.khan16@gmail.com>
2025-07-16 23:23:43 +0200

    add author_class

--- app/models/blorgh/article.rb
+++ app/models/blorgh/article.rb
@@ -1,16 +1,15 @@
 module Blorgh
   class Article < ApplicationRecord
     has_many :comments
-
     # Add the author association to the model
     attr_accessor :author_name
-    belongs_to :author, class_name: "User"
+    belongs_to :author, class_name: Blorgh.author_class.to_s
 
     before_validation :set_author
 
     private
       def set_author
-        self.author = User.find_or_create_by(name: author_name)
+        self.author = Blorgh.author_class.find_or_create_by(name: author_name)
       end
   end
 end
